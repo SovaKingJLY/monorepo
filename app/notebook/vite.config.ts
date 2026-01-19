4757010d8cb43f3a750b65f3beaaab44591c6ec5
主将从现 <12762017+jiang-lingyan@user.noreply.gitee.com>
2026-01-19 16:42:00 +0800

    debug:创建qiniu包示例

--- app/notebook/vite.config.ts
+++ app/notebook/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import uploadBundleQiniu from './plugins/uploadBundleQiniu';
+import uploadBundleQiniu from '@repo/qiniu';
 //import qiniu from 'vite-plugin-qiniu'; // 引入插件
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
